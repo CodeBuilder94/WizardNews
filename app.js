3ad557d1ebda171c5fbafe43108b338d69086743
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-01-05 11:44:46 -0600

    Filled out the html and linked the css to it.

--- app.js
+++ app.js
@@ -14,13 +14,27 @@ app.get('/',(req, res)=>{
 
   const html = `<!DOCTYPE html>
    <html>
-  <head> <title>Wizard News</title>
-   <head> 
-   <body>
-  <ul>
-  ${posts.map(post => `<li>${post.title}</1i>`)}
-   </ul> 
-  </body>
+    <head> 
+        <title>Wizard News</title>
+        <link rel="stylesheet" href="/style.css"/>
+    </head> 
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${posts.map(post => `
+          <div class="news-item">
+            <p>
+              <span class="news-position">${post.id}.▲</span>
+              ${post.title}
+              <small> (by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.data}
+            </small>
+          </div>
+        `).join("")}
+      </div>
+    </body>
   </html>`
 
   res.send (html)
