0da2811f6f52862c01e722b176c4038e3cfbace7
Matthew Smith <matthewsmith@Matthews-MacBook-Pro.local>
2023-01-05 12:06:06 -0500

    changes

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!!!!!"));
+app.get('/',(req, res)=>{
+  const posts = postBank.lists();
+
+  const html = `<!DOCTYPE html>
+   <html>
+  <head> <title>Wizard News</title>
+   <head> 
+   <body>
+  <ul>
+  ${posts.map(post => '<li>${post.title}</1i>')}
+   </ul> 
+  </body>
+  </html>`
+
+  res.send (html)
+});
 
 const PORT = 1337;
 
